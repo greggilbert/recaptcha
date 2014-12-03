43706751c797c7235cca58fb7a1beb8f49128bca
Tom Elliott <tom.w.elliott@gmail.com>
2014-12-03 18:37:08 -0500

    Add language and rendering option to script call. Add no script content from: https://developers.google.com/recaptcha/docs/faq#no_js

--- src/views/captchav2.blade.php
+++ src/views/captchav2.blade.php
@@ -3,10 +3,25 @@
 	var RecaptchaOptions = {{ json_encode($options) }};
 </script>
 @endif
-	    						<script src='https://www.google.com/recaptcha/api.js'></script>
-                                <div class="g-recaptcha" data-sitekey="{{ $public_key }}"></div>
+<script src='https://www.google.com/recaptcha/api.js?render=onload{{ (isset($lang) ? '&hl='.$lang : '') }}'></script>
+<div class="g-recaptcha" data-sitekey="{{ $public_key }}"></div>
 <noscript>
-	<iframe src="//www.google.com/recaptcha/api/noscript?k=<?php echo $public_key ?><?php echo (isset($lang) ? '&hl='.$lang : '') ?>" height="300" width="500" frameborder="0"></iframe><br>
-	<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-	<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+  <div style="width: 302px; height: 352px;">
+    <div style="width: 302px; height: 352px; position: relative;">
+      <div style="width: 302px; height: 352px; position: absolute;">
+        <iframe src="https://www.google.com/recaptcha/api/fallback?k={{ $public_key }}"
+                frameborder="0" scrolling="no"
+                style="width: 302px; height:352px; border-style: none;">
+        </iframe>
+      </div>
+      <div style="width: 250px; height: 80px; position: absolute; border-style: none;
+                  bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">
+        <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                  class="g-recaptcha-response"
+                  style="width: 250px; height: 80px; border: 1px solid #c1c1c1;
+                         margin: 0px; padding: 0px; resize: none;" value="">
+        </textarea>
+      </div>
+    </div>
+  </div>
 </noscript>
