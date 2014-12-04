7e4c952ee740f8365a72892e33697db417698e8d
Tom Elliott <tom.w.elliott@gmail.com>
2014-12-03 19:16:46 -0500

    Revert changes to original CheckRecaptcha class.

--- src/Greggilbert/Recaptcha/CheckRecaptcha.php
+++ src/Greggilbert/Recaptcha/CheckRecaptcha.php
@@ -9,8 +9,7 @@ class CheckRecaptcha
 {
     const SERVER		= 'http://www.google.com/recaptcha/api';
     const SERVER_SECURE	= 'https://www.google.com/recaptcha/api';
-    const ENDPOINT		= '/recaptcha/api/verify';
-	const ENDPOINTV2	= '/recaptcha/api/siteverify';
+	const ENDPOINT		= '/recaptcha/api/verify';
     const VERIFY_SERVER	= 'www.google.com';
 	
 	/**
@@ -27,17 +26,32 @@ class CheckRecaptcha
 			'challenge'		=> $challenge,
 			'response' => $response,
 		));
-		
-		var_dump($parameters);
-		
-		$curl = curl_init("https://www.google.com/recaptcha/api/siteverify?" . http_build_query($parameters));
-		curl_setopt($curl, CURLOPT_HEADER, false);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-		
-		$response = curl_exec($curl);
 
-		var_dump($response);
+		$http_request  = "POST " . self::ENDPOINT . " HTTP/1.0\r\n";
+		$http_request .= "Host: " . self::VERIFY_SERVER . "\r\n";
+		$http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
+		$http_request .= "Content-Length: " . strlen($parameters) . "\r\n";
+		$http_request .= "User-Agent: reCAPTCHA/PHP\r\n";
+		$http_request .= "\r\n";
+		$http_request .= $parameters;
+
+		$apiResponse = '';
+		
+		if (false == ($fs = @fsockopen(self::VERIFY_SERVER, 80)))
+		{
+			throw new \Exception('Could not open socket');
+		}
+
+		fwrite($fs, $http_request);
+
+		while (!feof($fs))
+		{
+			$apiResponse .= fgets($fs, 1160); // One TCP-IP packet
+		}
+		
+		fclose($fs);
+		
+		$apiResponse = explode("\r\n\r\n", $apiResponse, 2);
 
 		return explode("\n", $apiResponse[1]);
 	}
