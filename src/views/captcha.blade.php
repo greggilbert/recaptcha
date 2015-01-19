20ef14d0a7f3f165e1358f355f197bac6f6a73ff
Tuna Abutbul <tuna.abutbul@gmail.com>
2015-01-19 21:20:45 +0200

    Update captcha.blade.php

--- src/views/captcha.blade.php
+++ src/views/captcha.blade.php
@@ -3,9 +3,9 @@
 	var RecaptchaOptions = <?php echo json_encode($options) ?>;
 </script>
 @endif
-<script type="text/javascript" src="//www.google.com/recaptcha/api/challenge?k=<?php echo $public_key ?><?php echo (isset($lang) ? '&hl='.$lang : '') ?>"></script>
+<script type="text/javascript" src="//www.google.com/recaptcha/api/challenge?k=<?php echo $public_key ?><?php echo (isset($lang) ? '&amp;hl='.$lang : '') ?>"></script>
 <noscript>
-	<iframe src="//www.google.com/recaptcha/api/noscript?k=<?php echo $public_key ?><?php echo (isset($lang) ? '&hl='.$lang : '') ?>" height="300" width="500" frameborder="0"></iframe><br>
+	<iframe src="//www.google.com/recaptcha/api/noscript?k=<?php echo $public_key ?><?php echo (isset($lang) ? '&amp;hl='.$lang : '') ?>" height="300" width="500" frameborder="0"></iframe><br>
 	<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
 	<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
 </noscript>
