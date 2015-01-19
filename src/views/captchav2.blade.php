052d01e83aea40b76ebc9f269dcfce1c162557ab
Tuna Abutbul <tuna.abutbul@gmail.com>
2015-01-19 21:19:30 +0200

    Update captchav2.blade.php

--- src/views/captchav2.blade.php
+++ src/views/captchav2.blade.php
@@ -3,7 +3,7 @@
 	var RecaptchaOptions = {{ json_encode($options) }};
 </script>
 @endif
-<script src='https://www.google.com/recaptcha/api.js?render=onload{{ (isset($lang) ? '&hl='.$lang : '') }}'></script>
+<script src='https://www.google.com/recaptcha/api.js?render=onload{{ (isset($lang) ? '&amp;hl='.$lang : '') }}'></script>
 <div class="g-recaptcha" data-sitekey="{{ $public_key }}"></div>
 <noscript>
   <div style="width: 302px; height: 352px;">
